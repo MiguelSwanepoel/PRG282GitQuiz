c2d8988d9ac707a8ad7abc153892e2963ec24aea
MiguelSwanepoel <576798@student.belgiumcampus.ac.za>
2021-10-04 15:48:10 +0200

    Add Code As WellForm Design

--- PRG282GitQuiz/Form1.Designer.cs
+++ PRG282GitQuiz/Form1.Designer.cs
@@ -29,20 +29,46 @@ namespace PRG282GitQuiz
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lbxFile = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(146, 256);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(179, 43);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read File";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lbxFile
+            // 
+            this.lbxFile.FormattingEnabled = true;
+            this.lbxFile.Location = new System.Drawing.Point(12, 12);
+            this.lbxFile.Name = "lbxFile";
+            this.lbxFile.Size = new System.Drawing.Size(427, 238);
+            this.lbxFile.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 315);
+            this.ClientSize = new System.Drawing.Size(462, 329);
+            this.Controls.Add(this.lbxFile);
+            this.Controls.Add(this.btnRead);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.ListBox lbxFile;
     }
 }
 
--- PRG282GitQuiz/Form1.cs
+++ PRG282GitQuiz/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PRG282GitQuiz
 {
@@ -15,6 +16,39 @@ namespace PRG282GitQuiz
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader sr = new StreamReader("File.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lbxFile.Items.Add(line);
+                }
+                sr.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<String> Data = new List<string>();
+            Data.Add("Username: 007, Name: James Bond, Ocupation: Assassin");
+            Data.Add("Username: Stig, Name: The Stig, Ocupation: Reckless Driver");
+            Data.Add("Username: Beast, Name: Tendai Mtawarira, Ocupation: Prop");
+            Data.Add("UserName: Starlord, Name: Peter Quill, Ocupation: Galactic Idiot");
+            /*string fName = "File.txt";
+            if (File.Exists(fName))
+            {
+                File.Delete(fName);
+            }
+            File.Create(fName);
+            TextWriter tw = new StreamWriter(fName);
+            tw.WriteLine(Data);
+            tw.Close();*\
+
         }
     }
 }
